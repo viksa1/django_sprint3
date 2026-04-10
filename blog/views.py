5b3d294334f21a7b1478d13b03950a2ff2c4ac09
viksa1 <vikatyagunina@gmail.com>
2026-04-10 23:54:23 +0300

    Первый коммит: проект Django спринт 3

---
+++ blog/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from .models import Post, Category
+
+
+def index(request):
+    """Главная страница - 5 последних публикаций"""
+    posts = Post.objects.filter(
+        is_published=True,
+        pub_date__lte=timezone.now(),
+        category__is_published=True
+    ).order_by('-pub_date')[:5]
+    
+    return render(request, 'blog/index.html', {'posts': posts})
+
+
+def post_detail(request, post_id):
+    """Страница отдельной публикации"""
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        is_published=True,
+        pub_date__lte=timezone.now(),
+        category__is_published=True
+    )
+    return render(request, 'blog/detail.html', {'post': post})
+
+
+def category_posts(request, category_slug):
+    """Страница категории"""
+    category = get_object_or_404(
+        Category,
+        slug=category_slug,
+        is_published=True
+    )
+    
+    posts = Post.objects.filter(
+        category=category,
+        is_published=True,
+        pub_date__lte=timezone.now()
+    ).order_by('-pub_date')
+    
+    return render(request, 'blog/category.html', {
+        'category': category,
+        'posts': posts
+    })
